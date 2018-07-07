6a8d7d276c0f82000a001892fc944d86c196c5cc
Paul Adams <paadams@nmu.edu>
2018-07-07 13:28:04 -0500

    add vuex-class dep

---
+++ src/components/AppFormInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import {State, Action} from 'vuex-class'
+@Component
+export default class AppForm extends Vue {
+  @Prop() private msg!: string;
+  @State('fakeDataItems') fakeDataItems: any
+  @Action('generateFakeDataItem') generateFakeDataItem: any
+  created() {
+    console.log(this.fakeDataItems)
+  }
+  generate() {
+    this.generateFakeDataItem()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,44 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    fakeDataItem: {
+      type: <String>"",
+      recurrences: <Number>0,
+      body: <Object>{}
+    },
+    fakeDataItems: <any>[]
   },
   mutations: {
-
+    setFakeDataItemType(state, type: String) {
+      state.fakeDataItem.type = type;
+    },
+    setFakeDataItemRecurrences(state, recurrences: Number) {
+      state.fakeDataItem.recurrences = recurrences;
+    },
+    setFakeDataItemBody(state, body: Object) {
+      state.fakeDataItem.body = body;
+    },
+    addFakeDataItem(state) {
+      console.log(state.fakeDataItem);
+      state.fakeDataItems.push(state.fakeDataItem);
+    }
   },
   actions: {
-
+    setFakeDataItemType({ commit }, type: String) {
+      commit("setFakeDataType", type);
+    },
+    setFakeDataItemRecurrences({ commit }, recurrences: Number) {
+      commit("setFakeDataRecurrences", recurrences);
+    },
+    setFakeDataItemBody({ commit }, body: Object) {
+      commit("setFakeDataBody", body);
+    },
+    generateFakeDataItem({ commit }) {
+      commit("addFakeDataItem");
+    }
   }
-})
+});
